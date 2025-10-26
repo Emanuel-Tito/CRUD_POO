9ae72e2d3a75e77f091d6d239598fa105be2fa7a
Emanuel Tito <221703043+emanuel-tito0@users.noreply.github.com>
2025-10-25 21:13:23 -0500

    cambios.

--- prestamo/views.py
+++ prestamo/views.py
@@ -1,11 +1,7 @@
 from . forms import PrestamoForm
 from . models import Prestamo
-from django.views.generic import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView
-from django.views.generic.edit import DeleteView
 
 # Create your views here.
 """def prestamo_list(request):
